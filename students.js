f9ba410f0d489f3e0103391260a173e05af58140
tchua <timothy_chua@sfu.ca>
2022-06-17 08:23:53 +0000

    Update students.js

--- students.js
+++ students.js
@@ -1,113 +1,222 @@
-<!DOCTYPE html>
-<html>
-  <div class="headerContainer"><h1>STUDENT VISUALIZER</h1></div>
-  <div class="gridContainer">
-    <div class="box" id="box0"><p id="text0"></p></div>
-    <div class="box" id="box1"><p id="text1"></p></div>
-    <div class="box" id="box2"><p id="text2"></p></div>
-    <div class="box" id="box3"><p id="text3"></p></div>
-    <div class="box" id="box4"><p id="text4"></p></div>
-    <div class="box" id="box5"><p id="text5"></p></div>
-    <div class="box" id="box6"><p id="text6"></p></div>
-    <div class="box" id="box7"><p id="text7"></p></div>
-    <div class="box" id="box8"><p id="text8"></p></div>
-    <div class="box" id="box9"><p id="text9"></p></div>
-    <div class="box" id="box10"><p id="text10"></p></div>
-    <div class="box" id="box11"><p id="text11"></p></div>
-    <div class="box" id="box12"><p id="text12"></p></div>
-    <div class="box" id="box13"><p id="text13"></p></div>
-    <div class="box" id="box14"><p id="text14"></p></div>
-    <div class="box" id="box15"><p id="text15"></p></div>
-    <div class="box" id="box16"><p id="text16"></p></div>
-    <div class="box" id="box17"><p id="text17"></p></div>
-    <div class="box" id="box18"><p id="text18"></p></div>
-    <div class="box" id="box19"><p id="text19"></p></div>
-  </div>
-  <div>
-    <p id="notify"></p>
-  </div>
-  <div class="interfaceContainer">
-    <div class = "addContainer">
-      <h3>ADD STUDENT</h3><br><br>
-      <label for="addName">NAME: </label>
-			<input name = "addName" type="text" id="addName" maxlength="20"><br>
+const students = [];
 
-      <label for="addWeight">WEIGHT(kg): </label>
-			<input name="addWeight" type="number" id="addWeight" min=0 max=635><br>
+class Student {
+  constructor(name,weight,height,gpa,hair,alive) {
+    this.name = name;
+    this.weight = weight;
+    this.height = height;
+    this.gpa = gpa;
+    this.hair = hair;
+    this.alive = alive;
+  }
+}
+/*
+var student1 = new Student("Fred Flinstone",635,272,1,"DarkOrange","DECEASED");
+var student2 = new Student("Dead Velma", 100, 100, 0, "Brown","DECEASED");
 
-      <label for="addHeight">HEIGHT(cm): </label>
-			<input name="addHeight" type="number" id="addHeight" min=0 max=272><br>
+students.push(student1);
+//students.push(student2);
 
-      <label for="addGPA">GPA: </label>
-			<input name="addGPA" type="number"id="addGPA"><br>
+for (let i = 0; i < 19; i++) {
+  students.push(student1);
+}*/
+drawGrid();
 
-      <label for="addHair">HAIR: </label>
-			<select name="addHair" id="addHair"><br>
-        <option value="Black">BLACK</option>
-        <option value="White">WHITE</option>
-        <option value="Gray">GRAY</option>
-        <option value ="Brown">BROWN</option>
-        <option value="GoldenRod">BLONDE</option>
-        <option value="Red">RED</option>
-        <option value="DarkOrange">ORANGE</option>
-        <option value="Yellow">YELLOW</option>
-        <option value="Lime">GREEN</option>
-        <option value="Blue">BLUE</option>
-        <option value="Purple">PURPLE</option>
-      </select><br>
+function drawGrid() {
+  for (let i = 0; i < students.length; i++) {
+    var boxID = "box" + i;
+    var boxTextID = "text" + i;
 
-      <label for="addAlive">ALIVE: </label>
-			<select name="addAlive" id="addAlive"><br>
-        <option value="ALIVE">ALIVE</option>
-        <option value="DECEASED">DECEASED</option>
-      </select><br><br>
+    var currentBox = document.getElementById(boxID);
 
-      <button type="button" onclick="addStudent()">Submit</button>
-    </div>
+    if ((students[i].hair == "Black")||(students[i].hair == "Blue")||
+        (students[i].hair == "Brown")||(students[i].hair == "Purple")||
+        (students[i].hair == "Red")){
+      currentBox.style.color = "White";
+    }
+    else {
+      currentBox.style.color = "Black";
+    }
+    currentBox.style.backgroundColor = students[i].hair;
+    currentBox.style.height = students[i].height/2 + "%";
+    currentBox.style.width = students[i].weight/2 + "%";
+    currentBox.style.borderStyle = "groove";
 
-    <div class = "modifyContainer">
-      <h3>MODIFY STUDENT</h3><br>
-      <label for="modChoice">SELECT STUDENT: </label>
-			<select name="modChoice" id="modChoice" onchange="prepModForm()">
-      </select><br>
+    document.getElementById(boxTextID).innerHTML = students[i].name + "<br><br>" + students[i].gpa + "<br>" + students[i].alive;
+  }
+  document.getElementById("notify").innerHTML = "Use the visualizer interface below.";
+  prepDropList();
+  prepModForm();
+}
 
-      <label for="modName">NEW NAME: </label>
-			<input name = "modName" type="text" id="modName" maxlength="20"><br>
+function prepDropList() {
+  var dropList = document.getElementById("modChoice");
 
-      <label for="modWeight">NEW WEIGHT(kg): </label>
-			<input name="modWeight" type="number" id="modWeight" min=0 max=635><br>
+  for (let i = students.length; i >= 0; i--) {
+    dropList.remove(i);
+  }
+  for (let i = 0; i < students.length; i++) {
+    var newOption = document.createElement("option");
+    newOption.text = students[i].name;
+    dropList.add(newOption);
+  }
+  dropList.value = students[0].name;
+}
 
-      <label for="modHeight">NEW HEIGHT(cm): </label>
-			<input name="modHeight" type="number" id="modHeight" min=0 max=272><br>
+function prepModForm() {
+  var modSelect = document.getElementById("modChoice").value;
 
-      <label for="modGPA">NEW GPA: </label>
-			<input name="modGPA" type="number"id="modGPA"><br>
+  for (let i = 0; i < students.length; i++) {
+    if (modSelect == students[i].name) {
+      document.getElementById("modName").value = students[i].name;
+      document.getElementById("modWeight").value = students[i].weight;
+      document.getElementById("modHeight").value = students[i].height;
+      document.getElementById("modGPA").value = students[i].gpa;
+      document.getElementById("modHair").value = students[i].hair;
+      document.getElementById("modAlive").value = students[i].alive;
+      break;
+    }
+  }
+}
 
-      <label for="modHair">NEW HAIR: </label>
-			<select name="modHair" id="modHair"><br>
-        <option value="Black">BLACK</option>
-        <option value="White">WHITE</option>
-        <option value="Gray">GRAY</option>
-        <option value ="Brown">BROWN</option>
-        <option value="GoldenRod">BLONDE</option>
-        <option value="Red">RED</option>
-        <option value="DarkOrange">ORANGE</option>
-        <option value="Yellow">YELLOW</option>
-        <option value="Lime">GREEN</option>
-        <option value="Blue">BLUE</option>
-        <option value="Purple">PURPLE</option>
-      </select><br>
+function addStudent(){
+  var notifyText = "Student added to visualizer.";
 
-      <label for="modAlive">UPDATE ALIVE: </label>
-			<select name="modAlive" id="modAlive"><br>
-        <option value="ALIVE">ALIVE</option>
-        <option value="DECEASED">DECEASED</option>
-      </select><br><br>
+  var newName= document.getElementById("addName").value;
+  var newWeight= document.getElementById("addWeight").value;
+  var newHeight= document.getElementById("addHeight").value;
+  var newGPA= document.getElementById("addGPA").value;
+  var newHair= document.getElementById("addHair").value;
+  var newAlive= document.getElementById("addAlive").value;
 
-      <button type="button" onclick="deleteStudent()">Delete</button>
-      <button type="button" onclick="modStudent()">Modify</button>
-    </div>
-  </div>
-  <link rel="stylesheet" href = "students.css">
-  <script src="students.js"></script>
-</html>
+  if ((newName.length > 0) && (newWeight.length > 0) &&
+    (newHeight.length > 0) && (newGPA.length > 0)) {
+
+    if ((newWeight >=0) && (newWeight <= 635) &&
+      (newHeight >=0)&&(newHeight <= 272) &&
+      (newGPA >=0) && (newGPA <= 4)){
+      var dupeIndex = -1;
+
+      for (let i = 0; i < students.length; i++) {
+        if (newName == students[i].name) {
+          dupeIndex = i;
+          break;
+        }
+      }
+      if (dupeIndex === -1) {
+        
+        if (students.length < 20) {
+          var newStudent = new Student(newName,newWeight,newHeight,newGPA,newHair,newAlive);
+        
+          students.push(newStudent);
+          drawGrid();
+          
+          document.getElementById("addName").value = "";
+          document.getElementById("addWeight").value = "";
+          document.getElementById("addHeight").value = "";
+          document.getElementById("addGPA").value = "";
+
+          notifyText = "Added student to visualizer.";
+        }
+        else {
+          notifyText = "Cannot add student. Visualizer capacity reached (20/20).";
+        }
+      }
+      else {
+        notifyText = "Cannot add student. Names in visualizer must be unique.";
+      }
+    }
+    else{
+      notifyText =
+        "Cannot add student. One or more fields has humanely unobtainable values.";
+    }  
+  }
+  else {
+    notifyText = "Cannot add student. One or more fields left empty.";
+  }
+  document.getElementById("notify").innerHTML=notifyText;
+}
+
+function modStudent(){
+  var notifyText = "Modified student in visualizer.";
+  var selectForMod = document.getElementById("modChoice").value;
+
+  var studentToModIndex = -1;
+  for (let i = 0; i < students.length; i++) {
+    if (selectForMod === students[i].name) {
+      studentToModIndex = i;
+      break;
+    }
+  }
+  var newName= document.getElementById("modName").value;
+  var newWeight= document.getElementById("modWeight").value;
+  var newHeight= document.getElementById("modHeight").value;
+  var newGPA= document.getElementById("modGPA").value;
+  var newHair= document.getElementById("modHair").value;
+  var newAlive= document.getElementById("modAlive").value;
+
+  if ((newName.length > 0) && (newWeight.length > 0) &&
+    (newHeight.length > 0) && (newGPA.length > 0)) {
+
+    if ((newWeight >=0) && (newWeight <= 635) &&
+      (newHeight >=0)&&(newHeight <= 272) &&
+      (newGPA >=0) && (newGPA <= 4)){
+      var dupeIndex = -1;
+
+      for (let i = 0; i < students.length; i++) {
+        if (newName == students[i].name) {
+          dupeIndex = i;
+          break;
+        }
+      }
+      if ((dupeIndex === -1)||(dupeIndex === studentToModIndex)) {
+        students[studentToModIndex].name = newName;
+        students[studentToModIndex].weight = newWeight;
+        students[studentToModIndex].height = newHeight;
+        students[studentToModIndex].gpa = newGPA;
+        students[studentToModIndex].hair = newHair;
+        students[studentToModIndex].alive = newAlive;
+        drawGrid();
+      }
+      else {
+        notifyText = "Cannot modify student. Names in visualizer must be unique.";
+      }
+    }
+    else{
+      notifyText =
+        "Cannot modify student. One or more fields has humanely unobtainable values.";
+    }  
+  }
+  else {
+    notifyText = "Cannot modify student. One or more fields left empty.";
+  }
+  document.getElementById("notify").innerHTML=notifyText;
+}
+
+function deleteStudent() {
+  var notifyText = "Cannot delete from empty visualizer."
+  var selectForDelete = document.getElementById("modChoice").value;
+
+  for (let i = 0; i < students.length; i++) {
+    if (selectForDelete === students[i].name) {
+      clearGridEndBox();
+      students.splice(i,1);
+      drawGrid();
+      notifyText = "Student deleted from visualizer."
+      break;
+    }
+  }
+  document.getElementById("notify").innerHTML=notifyText
+  prepDropList();
+  prepModForm();
+}
+
+function clearGridEndBox() {
+  var deleteIndex = students.length -1;
+  var boxToClear = document.getElementById("box" + deleteIndex);
+  var textToClear = document.getElementById("text" + deleteIndex);
+
+  textToClear.innerHTML = "";
+  boxToClear.style.backgroundColor = "LightGrey";
+  boxToClear.style.borderStyle = "none";
+}
